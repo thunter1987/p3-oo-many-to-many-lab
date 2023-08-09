ee1c0390eafa20570948e9c1d2a65a2cd7652cf0
Tony Hunter <thunter1987@yahoo.com>
2023-08-08 22:32:19 -0500

    Passes Contract tests

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,63 @@
+from sqlite3 import Date
+
+
 class Author:
-    pass
+    def __init__(self, name):
+        self.name = name
 
 
 class Book:
-    pass
+    def __init__(self, title):
+        self.title = title
 
 
-class Contract:
-    pass
+class Contract (Author, Book):
+    def __init__(self, author, book, date, royalties):
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, value):
+        if isinstance(value, Author):
+            self._author = value
+        else:
+            raise Exception("Author must be an instance of Author")
+
+    @property
+    def book(self):
+        return self._book
+
+    @book.setter
+    def book(self, value):
+        if isinstance(value, Book):
+            self._book = value
+        else:
+            raise Exception("Book must be an instance of Book")
+
+    @property
+    def date(self):
+        return self._date
+
+    @date.setter
+    def date(self, value):
+        if isinstance(value, str):
+            self._date = value
+        else:
+            raise Exception("Date must be in correct format")
+
+    @property
+    def royalties(self):
+        return self._royalties
+
+    @royalties.setter
+    def royalties(self, value):
+        if isinstance(value, int):
+            self._royalties = value
+        else:
+            raise Exception("Roya;ties must be in correct format")
