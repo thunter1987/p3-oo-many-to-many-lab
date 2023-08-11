b83a7732070357d190259fc333d6012ff6d82496
Tony <thunter1987@yahoo.com>
2023-08-11 00:00:56 -0500

    passes tests

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,22 +1,48 @@
-from sqlite3 import Date
-
-
 class Author:
+    all = []
+    
     def __init__(self, name):
         self.name = name
+        Author.all.append(self)
+        
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.author == self]
+    
+    def books(self):
+        return [contract.book for contract in self.contracts()]
+    
+    def sign_contract(self, book, date, royalties):
+        return Contract(self, book, date, royalties)
+    
+    def total_royalties(self):
+        return sum(contract.royalties for contract in self.contracts())
 
 
 class Book:
+    all = []
+    
     def __init__(self, title):
         self.title = title
+        Book.all.append(self)
+        
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.book == self]
+    
+    def authors(self):
+        return [contract.author for contract in self.contracts()]
+
 
 
 class Contract (Author, Book):
+    all = []
+    
     def __init__(self, author, book, date, royalties):
         self.author = author
         self.book = book
         self.date = date
         self.royalties = royalties
+        Contract.all.append(self)
+
 
     @property
     def author(self):
@@ -61,3 +87,7 @@ class Contract (Author, Book):
             self._royalties = value
         else:
             raise Exception("Roya;ties must be in correct format")
+    
+    @classmethod  
+    def contracts_by_date(cls):
+        return sorted(cls.all, key=lambda d: d.date)
